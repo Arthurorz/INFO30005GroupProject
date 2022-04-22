c11d127086320f9723d6c305b1bb6c7d9b23afb4
ZhikaiWu2021 <zhikaiw1@student.unimelb.edu.au>
2022-04-22 15:43:45 +0800

    Update helpers.js

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -19,5 +19,34 @@ const helpers = {
         }else{
             return options.inverse(this);
         }
-    }
+    },
+    ifWeightOutOfRange:function(weight,lowerRange,higherRange, options){
+        if(weight > higherRange || weight < lowerRange){
+            return options.fn(this);
+        }else{
+            return options.inverse(this);
+        }
+    },
+    ifGlucoseOutOfRange:function(glucose,lowerRange,higherRange, options){
+        if(glucose > higherRange || glucose < lowerRange){
+            return options.fn(this);
+        }else{
+            return options.inverse(this);
+        }
+    },
+    ifInsulinOutOfRange:function(insulin,lowerRange,higherRange, options){
+        if(insulin > higherRange || insulin < lowerRange){
+            return options.fn(this);
+        }else{
+            return options.inverse(this);
+        }
+    },
+    ifExerciseOutOfRange:function(exercise,lowerRange,higherRange, options){
+        if(exercise > higherRange || exercise < lowerRange){
+            return options.fn(this);
+        }else{
+            return options.inverse(this);
+        }
+    },
+    
 }
