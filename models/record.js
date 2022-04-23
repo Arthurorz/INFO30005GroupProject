2384801e75f8f3e091a77fe77c57e77a83d6cdf7
ZhikaiWu2021 <zhikaiw1@student.unimelb.edu.au>
2022-04-23 14:46:16 +0800

    Update record.js

--- models/record.js
+++ models/record.js
@@ -2,29 +2,31 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
     patientid: {type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true},
-    glucose: {
-        status: {type: String, enum:["recorded", "unrecorded", "Not required"], default:"Unrecorded"},
-        value: {type: Number, default: 0},
-        comment: {type: String, default: ""},
-        date: {type: Date, default: null},
-    },
-    weight: {
-        status: {type: String, enum:["recorded", "unrecorded", "Not required"], default:"Unrecorded"},
-        value: {type: Number, default: 0},
-        comment: {type: String, default: ""},
-        date: {type: Date, default: null},
-    },
-    exercise: {
-        status: {type: String, enum:["recorded", "unrecorded", "Not required"], default:"Unrecorded"},
-        value: {type: Number, default: 0},
-        comment: {type: String, default: ""},
-        date: {type: Date, default: null},
-    },
-    insulin: {
-        status: {type: String, enum:["recorded", "unrecorded", "Not required"], default:"Unrecorded"},
-        value: {type: Number, default: 0},
-        comment: {type: String, default: ""},
-        date: {type: Date, default: null},
+    data:{
+        glucose: {
+            status: {type: String, enum:["recorded", "unrecorded", "Not required"], default:"Unrecorded"},
+            value: {type: Number, default: 0},
+            comment: {type: String, default: ""},
+            date: {type: Date, default: null},
+        },
+        weight: {
+            status: {type: String, enum:["recorded", "unrecorded", "Not required"], default:"Unrecorded"},
+            value: {type: Number, default: 0},
+            comment: {type: String, default: ""},
+            date: {type: Date, default: null},
+        },
+        exercise: {
+            status: {type: String, enum:["recorded", "unrecorded", "Not required"], default:"Unrecorded"},
+            value: {type: Number, default: 0},
+            comment: {type: String, default: ""},
+            date: {type: Date, default: null},
+        },
+        insulin: {
+            status: {type: String, enum:["recorded", "unrecorded", "Not required"], default:"Unrecorded"},
+            value: {type: Number, default: 0},
+            comment: {type: String, default: ""},
+            date: {type: Date, default: null},
+        }
     },
     date: {type: Date, default: null, required: true},
 })
