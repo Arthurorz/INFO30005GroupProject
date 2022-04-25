57a8ad66c1b2b30ce7231caca6d34e1f63337ec4
ZhikaiWu2021 <zhikaiw1@student.unimelb.edu.au>
2022-04-25 14:09:23 +0800

    update dashboard

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -8,17 +8,17 @@ const addPatient= async(req,res) =>{
     if(!patient){
         try{
             const patient = new Patient({
-                first_name: req.params.first_name,
-                last_name: req.params.last_name,
-                email: req.params.email,
-                password: req.params.password,
-                screen_name: req.params.screen_name,
-                yearofbirth: req.params.yearofbirth,
-                height: req.params.height,
-                brief_bio: req.params.brief_bio,
-                engagement: req.params.engagement,
-                photo: req.params.photo,
-                clinician :req.params.clinician
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                password: req.body.password,
+                screen_name: req.body.screen_name,
+                yearofbirth: req.body.yearofbirth,
+                height: req.body.height,
+                brief_bio: req.body.brief_bio,
+                engagement: req.body.engagement,
+                photo: req.body.photo,
+                clinician :req.body.clinician
             });
             await patient.save();
             console.log("Patient added");
@@ -30,6 +30,18 @@ const addPatient= async(req,res) =>{
         console.log("Patient id is : ", patient.id);
     }   
 }
+
+const test = async(req,res) => {
+    try{
+        const clinicianID = "626392e9a4d69d527a31780f"
+        const clinician = await Clinician.findById({clinicianID});
+        
+
+    }catch{
+
+    }
+}
+
 
 const renderClinicianData = async (req,res) => {
     try{
@@ -43,13 +55,14 @@ const renderClinicianData = async (req,res) => {
 const renderPatientData = async (req,res) => {
     try{
         const patient = await Patient.findOne({_id:req.params.id}).populate({
-            path:'clinician',
-            options:{lean:true}
-        }).lean().populate({
             path:'records',
-            options:{lean:true}
+            populate:{
+                path:'record_id',
+            }
         }).lean();
-        res.render('clinician-individualData.hbs',{layout: 'clinician.hbs',patientData:patient});
+        console.log(patient);
+        const records = patient.records;
+        res.render('clinician-individualData.hbs',{layout: 'clinician.hbs',patient:patient, records:records});
     }catch(err){
         console.log(err)
     }
@@ -57,18 +70,41 @@ const renderPatientData = async (req,res) => {
 
 const renderDashboard = async (req, res) => {
     try{
-        const patient = await Patient.find(/*{clinician_ID : req.params.clinicianID}*/).populate([{
-            path:'records',
-            options:{lean:true}
-        }]).lean().populate({
-            path:'clinician',
-            options:{lean:true}
-        }).lean();
+        const clinicianID = "626392e9a4d69d527a31780f";
+        const patients = (await Clinician.findById(clinicianID).populate({
+            path:'patients',
+            populate:{
+                path:'patient_id',
+                populate:{
+                    path:'records',
+                    options:{lean:true},
+                    populate:{
+                        path:'record_id',
+                        options:{lean:true},
+                    }
+                }
+            }
+        }).lean()).patients;
+        
 
-        console.log(patient);
-        if(patient.length > 0){
-            return res.render('clinician-dashboard.hbs',{layout:'clinician.hbs',patients:patient});
+        const patientList = [];
+        for(i in patients){
+            const patient = patients[i].patient_id;
+            for(j in patient.records){
+                const record = patient.records[j].record_id;
+                if (record.date == (new Date()).toDateString()){
+                    patientList.push({
+                        patient_id: patient._id,
+                        first_name: patient.first_name,
+                        bound:patient.bound,
+                        today_record: record,
+                    });
+                }
+            }
         }
+        
+        res.render('clinician-dashboard.hbs',{layout:'clinician.hbs',patientList:patientList});
+        
     }catch(err){
         console.log(err)
     }
@@ -76,16 +112,20 @@ const renderDashboard = async (req, res) => {
 
 const searchDashboard = async (req, res) => {
     try{
-        const result = await Patient.find({first_name : req.params.patientName}).populate({
-            path:'clinician',
-            options:{lean:true}
-        }).lean().populate({
-            path:'records',
-            options:{date:formatDate(new Date())}
-        });
-        console.log(result);
-        if(result.length > 0){
-            return res.redirect('clinician-dashboard.hbs',{layout:'clinician.hbs',patients:result});
+        if (req.body.patientName==''){
+            renderDashboard(req,res);
+        }else{
+            const result = await Patient.find({first_name : req.body.patientName}).populate({
+                path:'clinician',
+                options:{lean:true}
+            }).lean().populate({
+                path:'records',
+                options:{date:formatDate(new Date())}
+            });
+            console.log(result);
+            if(result.length > 0){
+                return res.render('clinician-dashboard.hbs',{layout:'clinician.hbs',patients:result});
+            }
         }
     }catch(err){
         console.log(err)
