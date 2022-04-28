347af201319757aed2a9d2d7c894ceb4b3e08ca0
ZhikaiWu2021 <zhikaiw1@student.unimelb.edu.au>
2022-04-28 21:10:28 +0800

    Revert "update heroku setting"
    This reverts commit 769d1bb6bfd68a31baaa30ad2af3a6c7570a771c.

--- models/db.js
+++ models/db.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
 // connect to mongodb
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL || "mongodb://localhost", {
-    dbName: "weballgood"
+mongoose.connect("mongodb+srv://ZhikaiWu:wzk13579wasd@weballgood.4a1d3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+    dbName:"weballgood"
     })
     .then(()=> console.log("Connected to mongodb"))
     .catch((err)=> console.log(err, "Error connecting to mongodb"));
