769d1bb6bfd68a31baaa30ad2af3a6c7570a771c
ZhikaiWu2021 <zhikaiw1@student.unimelb.edu.au>
2022-04-28 20:50:10 +0800

    update heroku setting

--- models/db.js
+++ models/db.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 // connect to mongodb
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://ZhikaiWu:wzk13579wasd@weballgood.4a1d3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
-    dbName:"weballgood"
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost", {
+    dbName: "weballgood"
     })
     .then(()=> console.log("Connected to mongodb"))
     .catch((err)=> console.log(err, "Error connecting to mongodb"));
