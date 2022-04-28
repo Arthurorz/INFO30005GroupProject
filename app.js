26191f12e9e4d122a048484be654f32d01bc0496
ZhikaiWu2021 <zhikaiw1@student.unimelb.edu.au>
2022-04-28 20:38:06 +0800

    change db to index

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 
 //connect to database
-require('./models/db');
+require('./models');
 
 Patient = require('./models/patient');
 Record = require('./models/record');
