bbd0bdbc887d201ac69d8ef7f0cbe21a01b40d0c
Zilunl <zilunli0730@outlook.com>
2022-04-28 22:37:57 +0800

    change some format

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 // connect to mongodb
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL,{
-    dbName:"weballgood" 
-    })
-    .then(()=> console.log("Connected to mongodb"))
-    .catch((err)=> console.log(err, "Error connecting to mongodb"));
+mongoose.connect(process.env.MONGO_URL || "mongodb+srv://ZhikaiWu:wzk35258970@weballgood.4a1d3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    dbName: "weballgood"
+})
+    .then(() => console.log("Connected to mongodb"))
+    .catch((err) => console.log(err, "Error connecting to mongodb"));
