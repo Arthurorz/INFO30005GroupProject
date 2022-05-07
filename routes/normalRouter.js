a6d97caa4a007886b147297a3e2f20a8161a7963
Zilunl <zilunli0730@outlook.com>
2022-05-07 22:02:02 +0800

    update login page

--- routes/normalRouter.js
+++ routes/normalRouter.js
@@ -5,7 +5,9 @@ constnoemalController = require("../controllers/normalController.js");
 normalRouter.get('/landingPage', (req, res) => res.render("normal-landingPage"));
 normalRouter.get('/aboutDiabetes', (req, res) => res.render("normal-aboutDia"));
 normalRouter.get('/aboutThisWeb', (req, res) => res.render("normal-aboutWeb"));
-normalRouter.get('/login', (req, res) => res.render("normal-login"));
-normalRouter.get('/forgetPassword', (req, res) => res.render("normal-forgetpass"));
+normalRouter.get('/patientLogin', (req, res) => res.render("normal-patientLogin"));
+normalRouter.get('/clinicianLogin', (req, res) => res.render("normal-clinicianLogin"));
+normalRouter.get('/patientForgetpass', (req, res) => res.render("normal-forgetpass"));
+normalRouter.get('/clinicianForgetpass', (req, res) => res.render("normal-forgetpass"));
 
 module.exports = normalRouter
